7c8252f2edf0e91fae3cabaa84b2e5a8ef2f46d4
Bharath <157495126+bhaarath22@users.noreply.github.com>
2025-03-29 17:50:13 +0530

    Update SR_testing_angles.ino

--- Quadruped-Robot/Acode/SR_testing_angles.ino
+++ Quadruped-Robot/Acode/SR_testing_angles.ino
@@ -3,7 +3,7 @@
 Servo servo[3];
 int Servopin[3]={2,3,4};
 void setup() {
-  // put your setup code here, to run once:
+  
 
 servo[0].attach(Servopin[0]);
 servo[1].attach(Servopin[1]);
@@ -11,7 +11,7 @@ servo[2].attach(Servopin[2]);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 servo[0].write(45);
 delay(1000);
 servo[0].write(135);
